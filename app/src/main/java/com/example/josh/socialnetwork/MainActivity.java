90b1e1c0faa46ebc32561886d5a5c8598fff850a
Joshua <joshmarion777@gmail.com>
2017-10-04 07:50:10 +0530

    I have created the new BottomNavigationView from external library to exclude the bottom animation and a new helper class for it

--- app/src/main/java/com/example/josh/socialnetwork/MainActivity.java
+++ app/src/main/java/com/example/josh/socialnetwork/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.josh.socialnetwork;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.example.josh.socialnetwork.Utils.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: This is the starting Bro..I'm being anti feminist");
+
+        setupBottomNavigationView();
     }
+
+    /**
+     * BottomNavigation view setup for no moving animation n stuff like that
+     */
+        private void setupBottomNavigationView(){
+            Log.d(TAG, "setupBottomNavigationView: setting the bottom navigation view");
+            BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+            BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        }
 }
+
---
+++ app/src/main/java/com/example/josh/socialnetwork/Utils/BottomNavigationViewHelper.java
@@ -0,0 +1,21 @@
+package com.example.josh.socialnetwork.Utils;
+
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+/**
+ * Created by JOSH on 04-10-2017.
+ */
+
+public class BottomNavigationViewHelper {
+    private static final String TAG = "BottomNavigationViewHel";
+
+    public static void setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx){
+        Log.d(TAG, "setupBottomNavigationView: setting up the bottom nav view");
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);
+    }
+}
