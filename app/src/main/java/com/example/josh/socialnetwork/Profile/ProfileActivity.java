bb825cebcda69c85f826155e6a9eb5e122da6f30
Joshua <joshmarion777@gmail.com>
2017-10-08 13:59:14 +0530

    Built the counter Box for the followers count n stuff like that but didn't link it

--- app/src/main/java/com/example/josh/socialnetwork/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/josh/socialnetwork/Profile/ProfileActivity.java
@@ -30,7 +30,7 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        //setupBottomNavigationView();
+        setupBottomNavigationView();
 
         setupToolBar();
     }
