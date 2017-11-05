55403f5602103ac51206775616c1fb1574f2e634
Joshua <josh2develop@gmail.com>
2017-11-05 20:52:06 +0530

    Camera Intent in Share Activity(Photo Fragment)

--- app/src/main/java/com/example/josh/socialnetwork/Utils/Permissions.java
+++ app/src/main/java/com/example/josh/socialnetwork/Utils/Permissions.java
@@ -13,4 +13,15 @@ public class Permissions {
             Manifest.permission.CAMERA,
             Manifest.permission.READ_EXTERNAL_STORAGE
     };
+
+    public static final String[] CAMERA_PERMISSION = {
+            Manifest.permission.CAMERA
+    };
+
+    public static final String[] WRITE_STORAGE_PERMISSION = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    public static final String[] READ_STORAGE_PERMISSION = {
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
 }
