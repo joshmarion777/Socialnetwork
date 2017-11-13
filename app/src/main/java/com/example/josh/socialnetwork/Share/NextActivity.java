b22d9321ee045f5e98c8ddeafb87256ba823eb96
Joshua <josh2develop@gmail.com>
2017-11-13 19:21:32 +0530

    Setting the image for sharing

--- app/src/main/java/com/example/josh/socialnetwork/Share/NextActivity.java
+++ app/src/main/java/com/example/josh/socialnetwork/Share/NextActivity.java
@@ -1,11 +1,25 @@
 package com.example.josh.socialnetwork.Share;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.josh.socialnetwork.R;
+import com.example.josh.socialnetwork.Utils.FirebaseMethods;
+import com.example.josh.socialnetwork.Utils.UniversalImageLoader;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by jbghostman on 13/11/17.
@@ -14,11 +28,113 @@ import com.example.josh.socialnetwork.R;
 public class NextActivity extends AppCompatActivity {
     private static final String TAG = "NextActivity";
 
+    //vars
+    private  String mAppend = "file:/";
+
+    //FireBase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
+    private FirebaseMethods mFirebaseMethods;
+
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
 
-        Log.d(TAG, "onCreate: got the chosen Image" + getIntent().getStringExtra(getString(R.string.selected_image)));
+        setupFirebaseAuth();
+
+        ImageView backArrow = findViewById(R.id.ivCloseShare);
+        // Used to Close Activity
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Closing the activity");
+                finish();
+            }
+        });
+
+        TextView share = findViewById(R.id.tvShare);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: navigating to the final share screen.");
+
+                //upload the image to firebase
+                setImage();
+            }
+        });
+    }
+
+    /**
+     * gets the image url from incoming intent and displays chosen image
+     */
+    private void setImage(){
+        Intent intent = getIntent();
+        ImageView image = findViewById(R.id.imageshare);
+        UniversalImageLoader.setImage(intent.getStringExtra(getString(R.string.selected_image)), image,null, mAppend );
+    }
+
+        /*
+    ------------------------------------------Firebase---------------------------------------------
+    */
+
+
+    /**
+     * Setup the firebase auth object
+     */
+
+    private void setupFirebaseAuth(){
+        Log.d(TAG, "setupFirebaseAuth: setting up firebase auth");
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
