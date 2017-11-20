e57b6a36389c5655d0542561ef20ca35d042b8b0
Joshua <josh2develop@gmail.com>
2017-11-20 16:47:13 +0530

    Sharing the Image using Camera p1 (Getting it ot )

--- app/src/main/java/com/example/josh/socialnetwork/Share/NextActivity.java
+++ app/src/main/java/com/example/josh/socialnetwork/Share/NextActivity.java
@@ -1,6 +1,7 @@
 package com.example.josh.socialnetwork.Share;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -34,6 +35,8 @@ public class NextActivity extends AppCompatActivity {
     private  String mAppend = "file:/";
     private int imageCount = 0;
     private String imgUrl;
+    private Intent intent;
+    private Bitmap bitmap;
 
     //widgets
     private EditText mCaption;
@@ -75,7 +78,18 @@ public class NextActivity extends AppCompatActivity {
                 //upload the image to firebase
                 Toast.makeText(NextActivity.this, "Attempting to upload new photo", Toast.LENGTH_SHORT).show();
                 String caption = mCaption.getText().toString();
-                mFirebaseMethods.uploadNewPhoto(getString(R.string.new_photo), caption, imageCount, imgUrl, null);
+                if (intent.hasExtra(getString(R.string.selected_image))){
+
+                    imgUrl = intent.getStringExtra(getString(R.string.selected_image));
+                    mFirebaseMethods.uploadNewPhoto(getString(R.string.new_photo), caption, imageCount, imgUrl, null);
+
+                }else if (intent.hasExtra(getString(R.string.selected_bitmap))){
+
+                    bitmap = intent.getParcelableExtra(getString(R.string.selected_bitmap));
+                    mFirebaseMethods.uploadNewPhoto(getString(R.string.new_photo), caption, imageCount, null, bitmap);
+                }
+
+
             }
         });
         setImage();
@@ -102,10 +116,20 @@ public class NextActivity extends AppCompatActivity {
      */
     private void setImage(){
         Log.d(TAG, "setImage: started");
-        Intent intent = getIntent();
+        intent = getIntent();
         ImageView image = findViewById(R.id.imageshare);
-        imgUrl = intent.getStringExtra(getString(R.string.selected_image));
-        UniversalImageLoader.setImage(imgUrl, image, null, mAppend);
+        if (intent.hasExtra(getString(R.string.selected_image))){
+            Log.d(TAG, "setImage: got new img url " + imgUrl);
+            imgUrl = intent.getStringExtra(getString(R.string.selected_image));
+            UniversalImageLoader.setImage(imgUrl, image, null, mAppend);
+
+        }else if (intent.hasExtra(getString(R.string.selected_bitmap))){
+
+            bitmap = intent.getParcelableExtra(getString(R.string.selected_bitmap));
+            Log.d(TAG, "setImage: got new bitmap");
+            image.setImageBitmap(bitmap);
+        }
+
     }
 
         /*
